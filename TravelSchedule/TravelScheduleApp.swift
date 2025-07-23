49dae02b4a651e26e202f80ea9c7dab2a9b2dc4a
Vladimir <bizin.vladimir01@gmail.com>
2025-07-23 15:34:49 +0300

    initial commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Vladimir on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
