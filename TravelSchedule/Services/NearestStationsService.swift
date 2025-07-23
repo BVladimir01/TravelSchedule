a33d3c8ade9271e11ca53ee5d00b41893d41133f
Vladimir <bizin.vladimir01@gmail.com>
2025-07-23 19:00:30 +0300

    feat(OpenAPI Services): add NearestStationService with protocol

---
+++ TravelSchedule/Services/NearestStationsService.swift
@@ -0,0 +1,28 @@
+//
+//  NearestTrainsService.swift
+//  TravelSchedule
+//
+//  Created by Vladimir on 23.07.2025.
+//
+
+typealias NearestStations = Components.Schemas.Stations
+
+
+protocol NearestStationsServiceProtocol {
+    func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations
+}
+
+
+struct NearestStationsService: NearestStationsServiceProtocol {
+    private let client: Client
+    private let apiKey: String
+    init(client: Client, apiKey: String) {
+        self.client = client
+        self.apiKey = apiKey
+    }
+    func getNearestStations(lat: Double, lng: Double, distance: Int) async throws -> NearestStations {
+        let query = Operations.getNearestStations.Input.Query(apikey: apiKey, lat: lat, lng: lng, distance: distance)
+        let response = try await client.getNearestStations(query: query)
+        return try response.ok.body.json
+    }
+}
