1e3cac50682cf8a2a388436a355225f08656e20c
Vladimir <bizin.vladimir01@gmail.com>
2025-07-23 19:11:12 +0300

    feat(OpenAPI Services): add validation of NearestStationsService method

--- TravelSchedule/Views/ContentView.swift
+++ TravelSchedule/Views/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import OpenAPIURLSession
 
 struct ContentView: View {
     var body: some View {
@@ -16,6 +17,28 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+//            getNearestStations()
+        }
+    }
+    
+    func getNearestStations() {
+        Task {
+            do {
+                let client = try Client(serverURL: Servers.Server1.url(), transport: URLSessionTransport())
+                let service = NearestStationsService(client: client, apiKey: "f5fad011-aeea-4dab-a7a8-872458a66b1f")
+                print("fetching stations...")
+                let stations = try await service.getNearestStations(
+                    lat: 59.864177,
+                    lng: 30.319163,
+                    distance: 50
+                )
+                print("stations:")
+                print(stations.stations, separator: "\n-------------------\n")
+            } catch {
+                print("error: \(error)")
+            }
+        }
     }
 }
 
