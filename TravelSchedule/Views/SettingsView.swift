86ecfd57a60ab38d880d28024a51f35f5843e353
Vladimir <bizin.vladimir01@gmail.com>
2025-07-30 13:14:35 +0300

    fix(settings view): wrap view in vstack to prevent extra tabs in tabview

--- TravelSchedule/Views/SettingsView.swift
+++ TravelSchedule/Views/SettingsView.swift
@@ -13,12 +13,14 @@ struct SettingsView: View {
     private let stubImageSize: Double = 223
     
     var body: some View {
-        Image(.serverError)
-            .resizable()
-            .frame(width: stubImageSize, height: stubImageSize)
-            .padding(.bottom, 16)
-        Text("Ошибка Сервера")
-            .font(.system(size: 24, weight: .bold))
+        VStack {
+            Image(.serverError)
+                .resizable()
+                .frame(width: stubImageSize, height: stubImageSize)
+                .padding(.bottom, 16)
+            Text("Ошибка Сервера")
+                .font(.system(size: 24, weight: .bold))
+        }
     }
     
 }
