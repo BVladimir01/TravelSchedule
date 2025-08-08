8ab0a4402eeb3ef587be31963173ba84498e4555
Vladimir <bizin.vladimir01@gmail.com>
2025-08-08 18:48:50 +0300

    feat(Stories): add ProgressSegmentView

---
+++ TravelSchedule/ScheduleSelectionFlow/Stories/Views/ProgressBarView/ProgressSegmentView.swift
@@ -0,0 +1,41 @@
+//
+//  ProgressSegmentView.swift
+//  StoriesViewPractice
+//
+//  Created by Vladimir on 06.08.2025.
+//
+
+import SwiftUI
+
+struct ProgressSegmentView: View {
+    
+    private let progress: Double
+    
+    init(progress: Double) {
+        self.progress = progress
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let radius = geometry.size.height
+            let width: Double = progress <= 0 ? 0 :
+                radius + max(min(progress, 1), 0)*(geometry.size.width - radius)
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: radius)
+                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: radius)
+                    .foregroundStyle(.blue)
+                    .frame(width: width)
+            }
+        }
+    }
+    
+}
+
+
+#Preview {
+    Color.black.overlay {
+        ProgressSegmentView(progress: 0.001)
+            .frame(width: 100, height: 10)
+    }
+}
