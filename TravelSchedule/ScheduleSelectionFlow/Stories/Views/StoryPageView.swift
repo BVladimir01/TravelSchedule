a22f4c4e788358a4f9e005a5d5958c0cd6697d8a
Vladimir <bizin.vladimir01@gmail.com>
2025-08-08 18:37:50 +0300

    feat(Stories): fill StoryPageView

--- TravelSchedule/ScheduleSelectionFlow/Stories/Views/StoryPageView.swift
+++ TravelSchedule/ScheduleSelectionFlow/Stories/Views/StoryPageView.swift
@@ -5,3 +5,33 @@
 //  Created by Vladimir on 08.08.2025.
 //
 
+import SwiftUI
+
+struct StoryPageView: View {
+    
+    private let story: StoryPageContent
+    
+    init(story: StoryPageContent) {
+        self.story = story
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Color.ypBlack.ignoresSafeArea()
+            Image(story.imageName)
+                .resizable()
+                .scaledToFit()
+            VStack(spacing: 16) {
+                Text(story.title)
+                    .foregroundStyle(.ypWhiteUniversal)
+                    .font(.system(size: 34, weight: .bold))
+                Text(story.text)
+                    .foregroundStyle(.ypWhiteUniversal)
+                    .font(.system(size: 20, weight: .regular))
+            }
+            .padding(.horizontal, 16)
+            .padding(.bottom, 40)
+        }
+    }
+    
+}
