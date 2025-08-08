4acdb1b7b30617f487085a13c7dba688e4c1cd6d
Vladimir <bizin.vladimir01@gmail.com>
2025-08-08 19:11:35 +0300

    fix(Stories): fill StoriesFlowView

--- TravelSchedule/ScheduleSelectionFlow/Stories/Views/StoriesFlowView.swift
+++ TravelSchedule/ScheduleSelectionFlow/Stories/Views/StoriesFlowView.swift
@@ -5,3 +5,46 @@
 //  Created by Vladimir on 08.08.2025.
 //
 
+import SwiftUI
+
+struct StoriesFlowView: View {
+    
+    @ObservedObject private var vm: StoriesFlowVM
+    
+    init(vm: StoriesFlowVM) {
+        self.vm = vm
+    }
+    
+    var body: some View {
+        ZStack(alignment: .top) {
+            StoryPageView(story: vm.currentStory.content)
+            ProgressBar(totalSegmentsNumber: vm.currentNumberOfDisplayedStories,
+                        currentSegmentIndex: vm.currentStoryLocalIndex,
+                        spacing: 6,
+                        currentProgress: vm.currentProgress)
+            .frame(height: 6)
+            .padding(.horizontal, 12)
+        }
+        .overlay(alignment: .topTrailing) {
+            closeButton
+                .frame(width: 30, height: 30)
+                .padding(.top, 50)
+                .padding(.trailing, 12)
+        }
+        .onAppear {
+            vm.viewAppeared()
+        }
+    }
+    
+    private var closeButton: some View {
+        Button {
+            vm.closeView()
+        } label: {
+            Image(.close)
+                .resizable()
+                .scaledToFit()
+        }
+        .contentShape(.circle)
+    }
+    
+}
--- TravelSchedule/ScheduleSelectionFlow/Stories/Views/StoriesView.swift
+++
@@ -1,7 +0,0 @@
-//
-//  StoriesView.swift
-//  TravelSchedule
-//
-//  Created by Vladimir on 08.08.2025.
-//
-
