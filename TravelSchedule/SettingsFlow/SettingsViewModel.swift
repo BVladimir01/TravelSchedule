5e413259094cca85df0e006d597e35de2ed5163d
Vladimir <bizin.vladimir01@gmail.com>
2025-08-11 17:35:12 +0300

    feat(Settings): add SettingsViewModel

---
+++ TravelSchedule/SettingsFlow/SettingsViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewModel.swift
+//  TravelSchedule
+//
+//  Created by Vladimir on 11.08.2025.
+//
+
+import SwiftUI
+import Combine
+
+final class SettingsViewModel: ObservableObject {
+    
+    @Published var isUsingDarkTheme: Bool
+    
+    private let themeStore: ColorSchemeStore
+    private var cancellable: Cancellable?
+    
+    init(themeStore: ColorSchemeStore) {
+        self.themeStore = themeStore
+        isUsingDarkTheme = themeStore.isDarkTheme
+        bindPublishedVarToStore()
+    }
+    
+    private func bindPublishedVarToStore() {
+        cancellable = $isUsingDarkTheme
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.isDarkTheme, on: themeStore)
+    }
+    
+}
